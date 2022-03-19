6afca468d8c03292eb12e6bf15dda2ca4339a398
Marlon Couto Ribeiro <84450525+marloncori@users.noreply.github.com>
2022-03-20 00:09:41 +0100

    Add files via upload

---
+++ modals.ts
@@ -0,0 +1,25 @@
+import { Item } from './models/item';
+
+export const ITEMS: Item[] = [
+    {
+      name: 'TakAway  ',
+      value: 'item_1',
+      price: 1.99,
+    },
+    {
+      name: 'Relay  ',
+      value: 'item_2',
+      price: 2.99,
+    },
+    {
+      name: 'Express  ',
+      value: 'item_3',
+      price: 3.99,
+    },
+    {
+      name: 'Direct  ',
+      value: 'item_4',
+      price: 4.99,
+    },
+  ];
+   
